ce2bfe5c8213a1388ea8c0892308f882fcc021a4
Romana Rust <rust@arch.ethz.ch>
2021-09-17 16:51:27 +0200

    Update create_glb_with_material.py

--- src/workshop_sjsu/create_glb_with_material.py
+++ src/workshop_sjsu/create_glb_with_material.py
@@ -6,10 +6,9 @@ from compas.datastructures import Mesh
 from compas.files import GLTFContent
 from compas.files import GLTFExporter
 from compas.files.gltf.data_classes import MaterialData, PBRMetallicRoughnessData
-from compas.files.gltf.extensions import KHR_materials_pbrSpecularGlossiness
 
 
-name = "test02"
+name = "test03"
 
 # 1. Load meshes
 filepath = os.path.join(DATA, "mesh_data.json")
@@ -29,24 +28,20 @@ scene = content.add_scene("half-sphere")
 # 2.a create material
 material = MaterialData()
 material.name = 'Plaster'
-material.pbr_metallic_roughness = PBRMetallicRoughnessData()
-material.pbr_metallic_roughness.base_color_factor = [
-    0.156862751, 0.831372559, 0.7764706, 1.0]
 material.double_sided = True
-pbr_specular_glossiness = KHR_materials_pbrSpecularGlossiness()
-pbr_specular_glossiness.diffuse_factor = [
-    0.156862751, 0.831372559, 0.7764706, 1.0]
-pbr_specular_glossiness.specular_factor = [0.0, 0.0, 0.0]
-pbr_specular_glossiness.glossiness_factor = 0.
-material.add_extension(pbr_specular_glossiness)
-content.add_material(material)
+material.pbr_metallic_roughness = PBRMetallicRoughnessData()
+material.pbr_metallic_roughness.base_color_factor = [0, 1, 0, 1.0]
+material.pbr_metallic_roughness.metallic_factor = 0.5
+material.pbr_metallic_roughness.roughness_factor = 0.1
+
+x = content.add_material(material)
 
 # 2.b add meshes
 for i, cmesh in enumerate(meshes):
     key = "line_%05d" % i
     node = content.add_node_to_scene(scene, node_name=key)
     mesh_data = content.add_mesh_to_node(node, cmesh)
-    # mesh_data.primitive_data_list[0].material = material # how to add material?
+    mesh_data.primitive_data_list[0].material = x # how to add material?
 
 # 3. Export GLTF
 filepath = os.path.join(os.path.join(WEB, "glb"), name + ".glb")
