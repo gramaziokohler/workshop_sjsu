8003e00d7dc89961920075674be0f5e5d447bbb4
Romana Rust <rust@arch.ethz.ch>
2021-09-17 14:31:50 +0200

    Update localhost.py

--- src/workshop_sjsu/localhost.py
+++ src/workshop_sjsu/localhost.py
@@ -4,11 +4,15 @@ import os
 
 PORT = 8000
 
-webdir = os.path.join(os.path.dirname(__file__), 'web')
-webdir = webdir.replace("\\","/")
+webdir = os.path.join(os.path.dirname(__file__), '..', '..', 'web')
+webdir = os.path.abspath(webdir)
+
+webdir = webdir.replace("\\", "/")
 os.chdir(webdir)
 
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
 httpd.serve_forever()
+
+# check out http://127.0.0.1:8000/example00.html
