5261e231c2ebf27e8f1f5c28bc1e7921ddc28853
Romana Rust <rust@arch.ethz.ch>
2021-09-23 08:42:25 +0200

    Update constants.py

--- src/workshop_sjsu/rhino/constants.py
+++ src/workshop_sjsu/rhino/constants.py
@@ -1,8 +1,8 @@
 import Rhino.Geometry as rg
 import math
 
-RADIUS = 0.2
+RADIUS = 0.15
 sphere = rg.Sphere(rg.Point3d(0,0,0), RADIUS)
-angle = 0.5 * math.pi
+angle = math.pi
 T = rg.Transform.Rotation(angle, rg.Vector3d.YAxis, sphere.Center)
 sphere.Transform(T)
