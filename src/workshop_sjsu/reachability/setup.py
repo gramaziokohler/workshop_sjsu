4a119d270cdc1d25549c4dbcc7c157b4a27db6c4
Romana Rust <rust@arch.ethz.ch>
2021-10-08 15:46:45 +0200

    Update setup.py

--- src/workshop_sjsu/reachability/setup.py
+++ src/workshop_sjsu/reachability/setup.py
@@ -2,14 +2,22 @@ import os
 import compas_fab
 from compas_fab.robots import Tool
 from compas_fab.robots import RobotSemantics
+from compas_fab.robots import PlanningScene
+from compas_fab.robots import CollisionMesh
+from compas_fab.backends import PyBulletClient
+from compas.datastructures import Mesh
+
 from workshop_sjsu import DATA
+from workshop_sjsu.ur.kinematics.analytical_inverse_kinematics import UR5AnalyticalIK
 
 
-def setup(client):
+def sjsu_setup(client):
     tool = Tool.from_json(os.path.join(DATA, "tool.json"))
 
-    urdf_filename = compas_fab.get('universal_robot/ur_description/urdf/ur5.urdf')
-    srdf_filename = compas_fab.get('universal_robot/ur5_moveit_config/config/ur5.srdf')
+    urdf_filename = compas_fab.get(
+        'universal_robot/ur_description/urdf/ur5.urdf')
+    srdf_filename = compas_fab.get(
+        'universal_robot/ur5_moveit_config/config/ur5.srdf')
 
     # TODO: convert to pybullet convex meshes?
 
@@ -22,4 +30,15 @@ def setup(client):
 
     # Attach tool and convert frames
     robot.attach_tool(tool)
-    return robot
+    scene = PlanningScene(robot)
+
+    camera_mesh = Mesh.from_json(os.path.join(DATA, "camera_in_position.json"))
+    cm = CollisionMesh(camera_mesh, 'camera')
+    scene.add_collision_mesh(cm)
+
+    return robot, scene
+
+
+class Client(PyBulletClient):
+    def inverse_kinematics(self, *args, **kwargs):
+        return UR5AnalyticalIK(self)(*args, **kwargs)
