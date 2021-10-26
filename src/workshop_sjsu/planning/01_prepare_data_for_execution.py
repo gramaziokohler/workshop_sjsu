561eb1506250e8c7705df68e5db058fea5f6eba1
Romana Rust <rust@arch.ethz.ch>
2021-10-25 18:44:58 -0700

    Update 01_prepare_data_for_execution.py

--- src/workshop_sjsu/planning/01_prepare_data_for_execution.py
+++ src/workshop_sjsu/planning/01_prepare_data_for_execution.py
@@ -16,7 +16,6 @@ from workshop_sjsu import DATA
 LOG.setLevel(logging.ERROR)
 
 
-
 def reduce_to_reachable(frames, connection_type='gui'):
 
     configurations = []
@@ -38,7 +37,9 @@ def reduce_to_reachable(frames, connection_type='gui'):
                                                         "check_collision": True, "cull": False})
                     solution = configs[IK_IDX]
                 except ValueError:
+                    #raise
                     solution = None
+                    print("ValueError")
                 if not solution:
                     break
                 else:
@@ -55,70 +56,93 @@ def reduce_to_reachable(frames, connection_type='gui'):
     return configurations, indices2keep
 
 
-
-
-
 def add_transition_between_paths_and_flatten(frames, gradients, colors, configurations, connection_type='gui'):
 
     frames_flattened = []
     gradients_flattened = []
     colors_flattened = []
     configurations_flattened = []
+    velocities = []
+    radii = []
 
     print(len(frames))
 
     with Client(connection_type=connection_type) as client:
         robot, _ = sjsu_setup(client)
 
-        for i, (path1, path2) in enumerate(zip(frames[:-1], frames[1:])):
+        if len(frames) == 1:  # one path
+            frames_flattened = frames[0]
+            gradients_flattened = gradients[0]
+            colors_flattened = colors[0]
+            configurations_flattened = configurations[0]
 
-            start_frame = path1[-1]
-            end_frame = path2[0]
-            transition_frames = create_transition_frames_on_sphere(start_frame, end_frame)
+        else:
+            for i, (path1, path2) in enumerate(zip(frames[:-1], frames[1:])):
 
-            # add another 2
-            # TODO: this can be improved by moving along the sphere to the point
-            #point_s = closest_point_on_plane(path1[-1].point, UP_PLANE)
-            #frame_s = Frame(point_s, path1[-1].xaxis, path1[-1].yaxis)
+                start_frame = path1[-1]
+                end_frame = path2[0]
+                transition_frames = create_transition_frames_on_sphere(
+                    start_frame, end_frame)
 
-            #point_e = closest_point_on_plane(path2[0].point, UP_PLANE)
-            #frame_e = Frame(point_e, path2[-1].xaxis, path2[-1].yaxis)
+                # add another 2
+                # TODO: this can be improved by moving along the sphere to the point
+                #point_s = closest_point_on_plane(path1[-1].point, UP_PLANE)
+                #frame_s = Frame(point_s, path1[-1].xaxis, path1[-1].yaxis)
 
-            if i == 0:
-                frames_flattened += path1
-                gradients_flattened += gradients[i]
-                colors_flattened += colors[i]
-                configurations_flattened += configurations[i]
+                #point_e = closest_point_on_plane(path2[0].point, UP_PLANE)
+                #frame_e = Frame(point_e, path2[-1].xaxis, path2[-1].yaxis)
 
-            # transition
-            frames_flattened += transition_frames #[frame_s, frame_e]
-            gradients_flattened += [0 for _ in range(len(transition_frames))]
-            colors_flattened += [(0, 0, 0) for _ in range(len(transition_frames))]
+                if i == 0:
+                    frames_flattened += path1
+                    gradients_flattened += gradients[i]
+                    colors_flattened += colors[i]
+                    configurations_flattened += configurations[i]
 
-            # configurations_flattened
-            for frame_tcf in transition_frames:
-                frame0_t0cf = robot.attached_tool.from_tcf_to_t0cf([frame_tcf])[0]
-                configs = client.inverse_kinematics(robot, frame0_t0cf, 
-                                                    options={
-                                                        "check_collision": True,
-                                                        "cull": False}
-                                                )
-                solution = configs[IK_IDX]
-                configurations_flattened.append(solution)
+                # transition
+                frames_flattened += transition_frames  # [frame_s, frame_e]
+                gradients_flattened += [
+                    0 for _ in range(len(transition_frames))]
+                colors_flattened += [(0, 0, 0)
+                                     for _ in range(len(transition_frames))]
 
-            # path2
-            frames_flattened += path2
-            gradients_flattened += gradients[i + 1]
-            colors_flattened += colors[i + 1]
-            configurations_flattened += configurations[i + 1]
+                # configurations_flattened
+                for frame_tcf in transition_frames:
+                    frame0_t0cf = robot.attached_tool.from_tcf_to_t0cf([frame_tcf])[
+                        0]
+                    configs = client.inverse_kinematics(robot, frame0_t0cf,
+                                                        options={
+                                                            "check_collision": True,
+                                                            "cull": False}
+                                                        )
+                    solution = configs[IK_IDX]
+                    configurations_flattened.append(solution)
+
+                # path2
+                frames_flattened += path2
+                gradients_flattened += gradients[i + 1]
+                colors_flattened += colors[i + 1]
+                configurations_flattened += configurations[i + 1]
 
     print("Now %d frames with transitions" % len(frames_flattened))
 
     # add offs to start and end
-    frames_flattened = [frames_flattened[0]] + frames_flattened + [frames_flattened[-1]]
-    gradients_flattened = [0] + gradients_flattened + [0]
-    colors_flattened = [(0, 0, 0)] + colors_flattened + [(0, 0, 0)]
-    configurations_flattened = [configurations_flattened[0]] + configurations_flattened + [configurations_flattened[-1]]
+    def add_offs(array_flattened, off=None, num=2):
+        if off is None:
+            s = array_flattened[0]
+            e = array_flattened[-1]
+        else:
+            s = off
+            e = off
+
+        start = [s for _ in range(num)]
+        end = [e for _ in range(num)]
+        return start + array_flattened + end
+
+    num = 2
+    frames_flattened = add_offs(frames_flattened, off=None, num=num)
+    gradients_flattened = add_offs(gradients_flattened, off=0, num=num)
+    colors_flattened = add_offs(colors_flattened, off=(0, 0, 0), num=num)
+    configurations_flattened = add_offs(configurations_flattened, off=None, num=num)
 
     return frames_flattened, gradients_flattened, colors_flattened, configurations_flattened
 
@@ -148,8 +172,8 @@ def make_configurations_smooth(configurations):
     # now try if we can bring all of them down
     for i, j in enumerate(zip(*new_joint_values)):
         v1 = min(j) + max(j)
-        v2 = v1 - 2 * math.pi
-        v3 = v1 + 2 * math.pi
+        v2 = min(j) - 2 * math.pi + max(j) - 2 * math.pi
+        v3 = min(j) + 2 * math.pi + max(j) + 2 * math.pi
         values = [math.fabs(v) for v in [v1, v2, v3]]
         idx = values.index(min(values))
         if idx == 1:
@@ -159,7 +183,8 @@ def make_configurations_smooth(configurations):
             for k in range(len(new_joint_values)):
                 new_joint_values[k][i] -= 2 * math.pi
 
-    new_configurations = [Configuration.from_revolute_values(j) for j in new_joint_values]
+    new_configurations = [Configuration.from_revolute_values(
+        j) for j in new_joint_values]
 
     return new_configurations
 
@@ -168,8 +193,23 @@ if __name__ == "__main__":
 
     #NAME = "example03"
     NAME = "example04"
+    NAME = "Juan Zavala Path"
+    NAME = "Juan_2"
+    NAME = "Milvia"
+    NAME = "Kayla"
 
-    filepath = os.path.join(DATA, "%s.json" % NAME)
+    filepath = os.path.join(DATA, 'current_file.txt')
+    with open(filepath, 'r') as f:
+        current = f.read()
+
+    basename = os.path.basename(current)
+    name, ending = os.path.splitext(basename)
+    name = name[:-len("_execution")]
+    
+    filepath = os.path.join(os.path.dirname(current), name + ending)
+
+
+    #filepath = os.path.join(DATA, "%s.json" % NAME)
 
     data = compas.json_load(filepath)
 
@@ -183,7 +223,8 @@ if __name__ == "__main__":
     # 2. Reduce to only use buildable paths
     ct = 'gui'
     #ct = 'direct'
-    configurations, indices2keep = reduce_to_reachable(frames, connection_type=ct)
+    configurations, indices2keep = reduce_to_reachable(
+        frames, connection_type=ct)
 
     # remove also from frames, gradients and colors
     frames = [frames[i] for i in indices2keep]
@@ -201,5 +242,6 @@ if __name__ == "__main__":
     data['colors'] = C
     data['configurations'] = J
 
-    filepath = os.path.join(DATA, "%s_execution.json" % NAME)
-    compas.json_dump(data, filepath)
+    #filepath = os.path.join(DATA, "%s_execution.json" % NAME)
+    compas.json_dump(data, current)
+    print(current)
